c0f07480d7511dcd1a3de2cbdc682bbf9186dc31
Marcin Hodor <mhodor@o2.pl>
2022-10-23 22:14:28 +0200

    fix misc-context types

--- context/misc-context.ts
+++ context/misc-context.ts
@@ -1,10 +1,20 @@
 import React, { Dispatch, SetStateAction } from "react";
 
-const MiscContext = React.createContext({
+interface MiscContext {
+  query: string;
+  setQuery: (query: string) => void;
+  chosenTags: string[];
+  setChosenTags: Dispatch<SetStateAction<string[]>>;
+  clickedId: string;
+  setClickedId: (id: string) => void;
+  resetFilters: () => void;
+}
+
+const MiscContext = React.createContext<MiscContext>({
   query: "",
   setQuery: (query: string) => {},
   chosenTags: <string[]>[],
-  setChosenTags: (tags: (tags: []) => string[]) => {},
+  setChosenTags: () => {},
   clickedId: "",
   setClickedId: (id: string) => {},
   resetFilters: () => {},

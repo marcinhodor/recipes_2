c16bdda1d1f267cabf9c711e389c35abc6084628
Marcin Hodor <mhodor@o2.pl>
2022-11-19 15:46:53 +0100

    test removing route guard component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,18 +4,18 @@ import Layout from "../components/Layout/Layout";
 import { AuthContextProvider } from "../context/auth-context";
 import { RecipesContextProvider } from "../context/recipes-context";
 import { MiscContextProvider } from "../context/misc-context";
-import ProtectRoute from "../components/HOC/ProtectRoute";
+// import ProtectRoute from "../components/HOC/ProtectRoute";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthContextProvider>
       <RecipesContextProvider>
         <MiscContextProvider>
-          <ProtectRoute>
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-          </ProtectRoute>
+          {/* <ProtectRoute> */}
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+          {/* </ProtectRoute> */}
         </MiscContextProvider>
       </RecipesContextProvider>
     </AuthContextProvider>

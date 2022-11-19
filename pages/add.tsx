8ac7e81afbe1fbae3c6bf04e9e4936a81ac92e9c
Marcin Hodor <mhodor@o2.pl>
2022-11-19 20:09:59 +0100

    add protected /add route

--- pages/add.tsx
+++ pages/add.tsx
@@ -1,9 +1,22 @@
 import type { NextPage } from "next";
-// import AddModifyForm from "../components/AddModifyForm";
+import { useRouter } from "next/router";
+import { useContext, useLayoutEffect } from "react";
+
+import AuthContext from "../context/auth-context";
+import MiscContext from "../context/misc-context";
 
 const Add: NextPage = () => {
-  //
-  // return <AddModifyForm />;
+  const router = useRouter();
+  const authCtx = useContext(AuthContext);
+  const miscCtx = useContext(MiscContext);
+
+  useLayoutEffect(() => {
+    if (!authCtx.isLoggedIn) {
+      router.push("/");
+      miscCtx.setShowNoAccessModal(true);
+    }
+  }, []);
+
   return <h1>add</h1>;
 };
 

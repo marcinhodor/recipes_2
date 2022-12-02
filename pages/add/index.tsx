f8bc7ab3ee97d79592fada7907cc4c3d1ee968e3
Marcin Hodor <mhodor@o2.pl>
2022-12-02 21:35:22 +0100

    remove useLayoutEffect

--- pages/add/index.tsx
+++ pages/add/index.tsx
@@ -1,23 +1,7 @@
 import type { NextPage } from "next";
-import { useRouter } from "next/router";
-import { useContext, useLayoutEffect } from "react";
 import AddModifyForm from "../../components/AddEditForm";
 
-import AuthContext from "../../context/auth-context";
-import MiscContext from "../../context/misc-context";
-
 const Add: NextPage = () => {
-  const router = useRouter();
-  const authCtx = useContext(AuthContext);
-  const miscCtx = useContext(MiscContext);
-
-  useLayoutEffect(() => {
-    if (!authCtx.isLoggedIn) {
-      router.push("/");
-      miscCtx.setShowNotifyModal({ show: true });
-    }
-  }, []);
-
   return <AddModifyForm />;
 };
 
--- pages/edit/[recipeId].tsx
+++ pages/edit/[recipeId].tsx
@@ -1,21 +1,9 @@
 import { useRouter } from "next/router";
-import { useContext, useLayoutEffect } from "react";
 import AddEditForm from "../../components/AddEditForm";
-import AuthContext from "../../context/auth-context";
-import MiscContext from "../../context/misc-context";
 
 const Modify = () => {
   const router = useRouter();
-  const authCtx = useContext(AuthContext);
-  const miscCtx = useContext(MiscContext);
   const recipeId = router.query.recipeId;
-
-  useLayoutEffect(() => {
-    if (!authCtx.isLoggedIn) {
-      router.push("/");
-      miscCtx.setShowNotifyModal({ show: true });
-    }
-  }, []);
 
   if (typeof recipeId === "string") {
     return <AddEditForm recipeId={recipeId} />;

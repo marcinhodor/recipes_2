f3453c4b2fe28ef516e579c3a57ac2a621c1f332
Marcin Hodor <mhodor@o2.pl>
2022-12-02 21:48:48 +0100

    mod footer

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 const Footer = () => {
   return (
     <footer className="flex justify-center w-full h-12 mt-2 text-sm border-t md:mt-4">
@@ -8,6 +10,7 @@ const Footer = () => {
         rel="noopener noreferrer"
       >
         Created by Marcin Hodor
+        <Image src="/link-svgrepo-com.svg" width={15} height={15} />
       </a>
     </footer>
   );

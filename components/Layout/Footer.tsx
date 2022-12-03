44ef9bdbdd3927c254f60f79b3c3fb4ba40b8909
Marcin Hodor <mhodor@o2.pl>
2022-12-03 17:34:38 +0100

    modify footer link target

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="flex justify-center w-full h-12 mt-2 text-sm border-t md:mt-4">
       <a
         className="flex items-center justify-center gap-2 hover:text-blue-600"
-        href="https://github.com/marcinhodor/"
+        href="https://github.com/marcinhodor/recipes_2"
         target="_blank"
         rel="noopener noreferrer"
       >
